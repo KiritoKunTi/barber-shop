cfc3bc3f72dc12aeed7727e17926295f2c1dacb5
Duman <duman200354@mail.ru>
2022-04-04 22:34:44 +0600

    header is created

--- barber/views.py
+++ barber/views.py
@@ -1,5 +1,9 @@
+from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
 def main(request):
     return render(request, 'barber/main.html')
+
+def services(request):
+    return render(request, 'barber/services.html')
